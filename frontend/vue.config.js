fd2a90458ea0ff1a10f7a89b0565879f035a360a
aadithya93 <aadithya93@gmail.com>
2025-06-04 20:05:10 -0700

    Update endpoints for create, load, add person and graph render

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+	transpileDependencies: true,
+	devServer: {
+		proxy: {
+			"^/api/v1": {
+				// Using ^ to match the start of the path
+				target: "http://localhost:8000", // Your FastAPI backend address
+				changeOrigin: true,
+			},
+		},
+	},
+});
